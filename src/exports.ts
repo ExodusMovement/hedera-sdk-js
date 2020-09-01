df39b33cf598d4af1834ca37f2aee03af7e35673
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-09-01 10:09:49 +0300

    slim: drop unused exports

--- src/exports.ts
+++ src/exports.ts
@@ -3,43 +3,15 @@
 export { AccountCreateTransaction } from "./account/AccountCreateTransaction";
 export { AccountUpdateTransaction } from "./account/AccountUpdateTransaction";
 export { AccountDeleteTransaction } from "./account/AccountDeleteTransaction";
-export { AccountInfoQuery, AccountInfo } from "./account/AccountInfoQuery";
 export { AccountBalanceQuery } from "./account/AccountBalanceQuery";
-export { AccountRecordsQuery } from "./account/AccountRecordsQuery";
-export { AccountStakersQuery } from "./account/AccountStakersQuery";
 
 export { CryptoTransferTransaction } from "./account/CryptoTransferTransaction";
 
-export { ContractCreateTransaction } from "./contract/ContractCreateTransaction";
-export { ContractDeleteTransaction } from "./contract/ContractDeleteTransaction";
-export { ContractExecuteTransaction } from "./contract/ContractExecuteTransaction";
-export { ContractUpdateTransaction } from "./contract/ContractUpdateTransaction";
-export { ContractFunctionResult } from "./contract/ContractFunctionResult";
-
-export { FileCreateTransaction } from "./file/FileCreateTransaction";
-export { FileDeleteTransaction } from "./file/FileDeleteTransaction";
-export { FileUpdateTransaction } from "./file/FileUpdateTransaction";
-export { FileAppendTransaction } from "./file/FileAppendTransaction";
-export { FileInfoQuery } from "./file/FileInfoQuery";
-export { FileContentsQuery } from "./file/FileContentsQuery";
-
-export { ContractCallQuery } from "./contract/ContractCallQuery";
-export { ContractRecordsQuery } from "./contract/ContractRecordsQuery";
-export { ContractInfoQuery } from "./contract/ContractInfoQuery";
-export { ContractBytecodeQuery } from "./contract/ContractBytecodeQuery";
 export { TransactionReceiptQuery } from "./TransactionReceiptQuery";
-export { TransactionRecordQuery } from "./TransactionRecordQuery";
-export { SystemDeleteTransaction } from "./SystemDeleteTransaction";
-export { SystemUndeleteTransaction } from "./SystemUndeleteTransaction";
-export { FreezeTransaction } from "./FreezeTransaction";
-export { GetBySolidityIdQuery } from "./GetBySolidityIdQuery";
-
-export { NetworkVersionInfoQuery, NetworkVersionInfo, SemanticVersion } from "./NetworkVersionInfoQuery";
 
 export { Transaction } from "./Transaction";
 
 export { Status } from "./Status";
-export { Transfer } from "./Transfer";
 
 // Errors
 export { HederaStatusError } from "./errors/HederaStatusError";
@@ -49,22 +21,10 @@ export { HbarRangeError } from "./errors/HbarRangeError";
 export { MaxQueryPaymentExceededError } from "./errors/MaxQueryPaymentExceededError";
 export { HederaPrecheckStatusError } from "./errors/HederaPrecheckStatusError";
 export { HederaReceiptStatusError } from "./errors/HederaReceiptStatusError";
-export { HederaRecordStatusError } from "./errors/HederaRecordStatusError";
-
-// Deprecated Errors
-export { ResponseCode, HederaError } from "./errors/HederaError";
-export { ValidationError } from "./errors/ValidationError";
-export { TinybarValueError } from "./errors/TinybarValueError";
-export { MaxPaymentExceededError } from "./errors/MaxPaymentExceededError";
 
 export { Ed25519PrivateKey } from "./crypto/Ed25519PrivateKey";
 export { Ed25519PublicKey } from "./crypto/Ed25519PublicKey";
-export { ThresholdKey } from "./crypto/ThresholdKey";
 export { PublicKey } from "./crypto/PublicKey";
-export { KeyList } from "./crypto/KeyList";
-export { Mnemonic } from "./crypto/Mnemonic";
-export { MnemonicValidationResult } from "./crypto/MnemonicValidationResult";
-export { MnemonicValidationStatus } from "./crypto/MnemonicValidationStatus";
 export { KeyMismatchError } from "./crypto/KeyMismatchError";
 
 export { Hbar } from "./Hbar";
@@ -76,7 +36,6 @@ export { FileId } from "./file/FileId";
 export { TransactionId } from "./TransactionId";
 
 export { TransactionReceipt } from "./TransactionReceipt";
-export { TransactionRecord } from "./TransactionRecord";
 
 export { ContractFunctionParams } from "./contract/ContractFunctionParams";
 export { Time } from "./Time";
@@ -84,11 +43,4 @@ export { Time } from "./Time";
 export { TransactionSigner } from "./BaseClient";
 
 // Consensus
-export { ConsensusTopicCreateTransaction } from "./consensus/ConsensusTopicCreateTransaction";
-export { ConsensusTopicDeleteTransaction } from "./consensus/ConsensusTopicDeleteTransaction";
-export { ConsensusTopicUpdateTransaction } from "./consensus/ConsensusTopicUpdateTransaction";
-export { ConsensusTopicInfoQuery } from "./consensus/ConsensusTopicInfoQuery";
 export { ConsensusTopicId } from "./consensus/ConsensusTopicId";
-export { ConsensusSubmitMessageTransaction } from "./consensus/ConsensusSubmitMessageTransaction";
-export { ConsensusMessageSubmitTransaction } from "./consensus/ConsensusMessageSubmitTransaction";
-
