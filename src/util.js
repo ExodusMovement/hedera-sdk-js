4f9d99014e01fcbb95a71ac4aaea02dd0f04f801
Сковорода Никита Андреевич <chalkerx@gmail.com>
2021-12-02 06:03:33 +0300

    fix: improvements to DataView logic
    Signed-off-by: Сковорода Никита Андреевич <chalkerx@gmail.com>

--- src/util.js
+++ src/util.js
@@ -308,6 +308,6 @@ export function convertToNumber(variable) {
  */
 export function safeView(arr, offset = 0, length = arr.byteLength) {
     if (!(Number.isInteger(offset) && offset >= 0)) throw new Error('Invalid offset!')
-    if (!(Number.isInteger(length) && length > 0)) throw new Error('Invalid length!')
+    if (!(Number.isInteger(length) && length >= 0)) throw new Error('Invalid length!')
     return new DataView(arr.buffer, arr.byteOffset + offset, Math.min(length, arr.byteLength - offset));
 }
